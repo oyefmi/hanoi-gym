7c6c4744a8af0aba541a0c51fee3457398969f65
oyefmi <31611566+oyefmi@users.noreply.github.com>
2019-05-25 11:05:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 setup(name = 'hanoi_gym',
       version = '0.0.1',
+      packages = find_packages()
       author = 'Michael Oyefusi',
       url = "https://github.com/oyefmi/hanoi-gym",
       install_requires = ['numpy', 'gym']
