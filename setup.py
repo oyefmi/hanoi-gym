c06b9d457644ff39ff4f4bfcbd271f812cf8a33c
oyefmi <31611566+oyefmi@users.noreply.github.com>
2019-05-25 09:54:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 setup(name = 'hanoi_gym',
       version = '0.0.1',
-      author = Michael Oyefusi,
+      author = 'Michael Oyefusi',
       url = "https://github.com/oyefmi/hanoi-gym",
       install_requires = ['numpy', 'gym']
 )
