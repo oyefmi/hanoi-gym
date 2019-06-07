c45c95239f0617bfe8720410450f3e12e506d1f9
Michael Oyefusi <31611566+oyefmi@users.noreply.github.com>
2019-06-07 09:09:18 -0400

    Update hanoi_DQN.py

--- hanoi_DQN.py
+++ hanoi_DQN.py
@@ -16,7 +16,7 @@ class Agent:
         self.state_size = 3
         self.action_size = len(action_size)
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.8    # discount rate
+        self.gamma = 0.8  # discount rate
         self.epsilon = .95  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
@@ -30,10 +30,9 @@ class Agent:
         model = Sequential()
 
         model.add(Dense(50, input_dim=1, activation='relu'))
-        # model.add(Dense(80, activation='relu'))
         model.add(Dense(100, activation='relu'))
-        model.add(Dense(25, activation='relu'))
-        model.add(Dense(6, activation = 'linear'))
+        model.add(Dense(50, activation='relu'))
+        model.add(Dense(6, activation='linear'))
         model.compile(loss='mse', optimizer=SGD(lr=self.learning_rate))
 
         return model
@@ -44,7 +43,7 @@ class Agent:
         st = np.array(state)
 
         self.memory.append((st, action, reward, nxt, done))
-    
+
     # decides how the agent will act based on the epsilon greedy policy
     def act(self, state):
         st = np.array(state)
@@ -89,63 +88,64 @@ class Agent:
 
 
 if __name__ == "__main__":
-        env = gym.make('Hanoi-v0')
-        action_size = env.observation_space
-        state_size = env.action_space
-        dqn_hanoi = Agent(state_size, action_size)
-        num_moves = 0
-        episodes = 100
-        test = 15
+    env = gym.make('Hanoi-v0')
+    action_size = env.observation_space
+    state_size = env.action_space
+    dqn_hanoi = Agent(state_size, action_size)
+    num_moves = 0
+    episodes = 100
+    test = 15
+    done = False
+    training_list = []
+    test_list = []
+
+    # train model while iteratively updating q_values
+    for t in range(episodes):
+        num_moves = 0  # the total number of moves it takes to solve the puzzle
+        state = env.reset()
         done = False
-        training_list = []
-        test_list = []
 
-        # train model while iteratively updating q_values
-        for t in range(episodes):
-            num_moves = 0  # the total number of moves it takes to solve the puzzle
-            state = env.reset()
-            done = False
+        while not done:
+            num_moves += 1
+            action = dqn_hanoi.act(state)
+            next_state, reward, done = env.step(action)
+            dqn_hanoi.remember(state, action, reward, next_state, done)
+            state = next_state
 
-            while not done:
-                num_moves += 1
-                action = dqn_hanoi.act(state)
-                next_state, reward, done = env.step(action)
-                dqn_hanoi.remember(state, action, reward, next_state, done)
-                state = next_state
-
-                # update weights and values after each episode
-                if done:
-                    print("Number of moves to solve: " + str(num_moves))
-                    training_list.append(num_moves)
-                    dqn_hanoi.replay()
-                    dqn_hanoi.target_train()
-
-                if num_moves > 250:
-                    if num_moves % 10 == 0:
-                        dqn_hanoi.save_model("Episode-{}.model".format(t))
-                else:
-                    dqn_hanoi.save_model("success.model")
-
-        training_average = sum(training_list) / episodes
-        print("\n" + "Average number of moves for training model " + str(training_average)
-              + "\n" + "\n")
-
-        # test model performance
-        for t in range(test):
-            num_moves = 0  # the total number of moves it takes to solve the puzzle
-            state = env.reset()
-            done = False
-
-            while not done:
-                num_moves += 1
-                action = dqn_hanoi.act(state)
-                next_state, reward, done = env.step(action)
-                dqn_hanoi.remember(state, action, reward, next_state, done)
-                state = next_state
-
+            # update weights and values after each episode
             if done:
                 print("Number of moves to solve: " + str(num_moves))
-                test_list.append(num_moves)
+                training_list.append(num_moves)
+                dqn_hanoi.replay()
+                dqn_hanoi.target_train()
 
-        test_average = sum(test_list) / test
-        print("\n" + "Average number of moves for test model " + str(test_average)) 
+            if num_moves > 250:
+                if num_moves % 10 == 0:
+                    dqn_hanoi.save_model("Episode-{}.model".format(t))
+            else:
+                dqn_hanoi.save_model("success.model")
+
+    training_average = sum(training_list) / episodes
+    print("\n" + "Average number of moves for training model " + str(training_average)
+          + "\n" + "\n")
+
+    # test model performance
+    for t in range(test):
+        num_moves = 0  # the total number of moves it takes to solve the puzzle
+        state = env.reset()
+        done = False
+
+        while not done:
+            num_moves += 1
+            action = dqn_hanoi.act(state)
+            next_state, reward, done = env.step(action)
+            dqn_hanoi.remember(state, action, reward, next_state, done)
+            state = next_state
+
+        if done:
+            print("Number of moves to solve: " + str(num_moves))
+            dqn_hanoi.target_train()
+            test_list.append(num_moves)
+
+    test_average = sum(test_list) / test
+    print("\n" + "Average number of moves for test model " + str(test_average)) 
